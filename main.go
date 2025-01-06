056dd3af03585893adfb3da0efea21e1143559c8
adrichey <1534554+adrichey@users.noreply.github.com>
2025-01-06 16:17:14 -0600

    Add input validation and secret key validation

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/adrichey/vigenere-cipher-golang/vigenereCipher"
 )
 
 func main() {
-	vigenereCipher := vigenereCipher.NewCipher()
+	vigenereCipher, err := vigenereCipher.NewCipher("THISISTHEKEY")
 
-	vigenereCipher.PrintLetterMap()
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: Fetch this input from an input file eventually
+	input := "We attack at dawn We attack with kindness"
+	output, err := vigenereCipher.Encode(input)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(output)
 }
--- vigenereCipher/vigenereCipher.go
+++ vigenereCipher/vigenereCipher.go
@@ -1,18 +1,16 @@
 package vigenereCipher
 
-import "fmt"
-
-const characters string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+import (
+	"errors"
+)
 
 type Cipher struct {
 	letterMap map[string]int
-}
-
-func (c *Cipher) PrintLetterMap() {
-	fmt.Println(c.letterMap, len(c.letterMap))
+	secretKey []byte
 }
 
 func (c *Cipher) createLetterMap() {
+	characters := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	c.letterMap = make(map[string]int)
 	for k, v := range characters {
 		letter := string(v)
@@ -20,9 +18,44 @@ func (c *Cipher) createLetterMap() {
 	}
 }
 
-func NewCipher() *Cipher {
+func (c *Cipher) Encode(input string) (string, error) {
+	err := c.validateInput([]byte(input))
+
+	if err != nil {
+		return "", err
+	}
+
+	// TODO - Implement convert text with cipher here
+	return "ENCODED TEXT WILL APPEAR HERE", nil
+}
+
+func (c *Cipher) validateInput(s []byte) error {
+	for _, b := range s {
+		if !('a' <= b && b <= 'z') &&
+			!('A' <= b && b <= 'Z') &&
+			b != '\r' &&
+			b != '\n' &&
+			b != '\t' &&
+			b != ' ' {
+			return errors.New("input is invalid. It must only contain characters A-Z and spaces.")
+		}
+	}
+
+	return nil
+}
+
+func NewCipher(secretKey string) (*Cipher, error) {
+	// Validate the secretKey to make sure we can use it with this cipher
+	sk := []byte(secretKey)
+	for _, b := range sk {
+		if !('a' <= b && b <= 'z') && !('A' <= b && b <= 'Z') {
+			return nil, errors.New("secretKey is invalid while creating new Cipher instance. It must only contain characters A-Z.")
+		}
+	}
+
 	c := &Cipher{}
 	c.createLetterMap()
+	c.secretKey = sk
 
-	return c
+	return c, nil
 }
